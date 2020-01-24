78c4228b7931dda7072dd050c65ff30065eed15c
Han Tu <htu@breakfreesolutions.com>
2020-01-24 22:29:03 +0000

    use hiera as ENC

--- manifests/site.pp
+++ manifests/site.pp
@@ -31,6 +31,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  hiera_include('classes')
 }
 
 node 'bananabruise1c.mylabserver.com' {
