d7fb4c104ba3afbbcf603b5609e2b25f55a893c9
BananaBruise <58489489+BananaBruise@users.noreply.github.com>
2020-01-24 16:17:49 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -24,7 +24,7 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node /*bananabruise4c/ {
+node 'bananabruise4c' {
   notify {'this node is overriden by site.pp':}
 }
 node default {
